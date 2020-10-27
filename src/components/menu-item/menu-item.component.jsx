b737c469232f61232d2482e8cf47e00cd5973d89
Shadi Domat <shadidomat@gmail.com>
2020-10-27 11:50:26 +0100

    Updating app with a shop page, collection preview component, collection item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -6,8 +6,12 @@ import './menu-item.styles.scss';
 const MenuItem = ({ title, imageUrl, size, linkUrl, match, history }) => (
   <div 
     className={`${size || ''} menu-item`} 
-    onClick={() => history.push(`${match.url}${linkUrl}`)}>
-    <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div 
+      className="background-image" 
+      style={{ backgroundImage: `url(${imageUrl})` }} 
+    />
     <div className='content'>
       <h1 className='title'>{title.toUpperCase()}</h1> 
       <span className='subtitle'>SHOP NOW</span>
